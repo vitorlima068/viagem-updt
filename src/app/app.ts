6562d0dc8fc7d765b85a2cf3bf2a6e932a186a0b
vitorlima068 <vitor.g.lima18@aluno.senai.br>
2026-04-01 10:54:48 -0500

    new

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './menu/menu'
+import { Card } from './card/card'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
