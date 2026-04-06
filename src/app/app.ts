febe736997341611f8b3eda0c22f30b79d078cf1
vitorlima068 <vitor.g.lima18@aluno.senai.br>
2026-04-06 11:01:50 -0500

    atualização

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './menu/menu'
 import { Card } from './card/card'
+import { Locais } from './locais/locais'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, Card],
+  imports: [RouterOutlet, Menu, Card, Locais],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/locais/locais.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-locais',
+  imports: [],
+  templateUrl: './locais.html',
+  styleUrl: './locais.css',
+})
+export class Locais {}
